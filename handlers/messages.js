01481e00a9859daa9d118a71027f94722bf4793e
khaled9819627 <khaledking20099@gmail.com>
2025-06-13 09:58:45 +0300

    Update messages.js

--- handlers/messages.js
+++ handlers/messages.js
@@ -2,6 +2,7 @@ const config = require('../config');
 const logger = require('../utils/console');
 const { loadPlugins } = require('./plugins');
 const crypto = require('crypto');
+const { isSpamming } = require('../utils/antiSpam');
 
 async function handleMessages(sock, { messages }) {
     if (!messages || !messages[0]) return;
