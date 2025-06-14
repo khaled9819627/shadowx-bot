74b8b3e553907f17538d94faa6e7807d3cb49729
khaled9819627 <khaledking20099@gmail.com>
2025-06-14 12:49:34 +0300

    Create export.js

---
+++ handlers/export.js
@@ -0,0 +1,11 @@
+const { handleCommand, handleMessages, cmd, commands } = require('./handler');
+const { loadPlugins } = require('./plugins');
+
+
+module.exports = {
+    handleCommand,     
+    handleMessages,    
+    loadPlugins,      
+    cmd,              
+    commands          
+};
